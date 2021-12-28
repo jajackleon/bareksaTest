2f78641adb205c4177dbffc14051af76b6ba8f49
Jackie Leonardy <jackiezheng2024@gmail.com>
2021-12-28 17:02:36 +0700

    initial Commit

---
+++ testBareksa/testBareksa/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  testBareksa
+//
+//  Created by Jackie Leonardy on 28/12/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
