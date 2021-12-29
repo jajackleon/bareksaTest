a24246a8e867fee17a15a83cbdb200f5946356a9
Jackie Leonardy <jackiezheng2024@gmail.com>
2021-12-29 23:04:46 +0700

    Generic Networking

---
+++ testBareksa/testBareksa/Utils/Constants.swift
@@ -0,0 +1,13 @@
+//
+//  Constans.swift
+//  testBareksa
+//
+//  Created by Jackie Leonardy on 29/12/21.
+//
+
+import Foundation
+
+struct Constants {
+    static let productDetailURL = URL(string: "https://ae1cdb19-2532-46fa-9b8f-cce01702bb1e.mock.pstmn.io/takehometest/apps/compare/detail?productCodes=KI002MMCDANCAS00&productCodes=TP002EQCEQTCRS00&productCodes=NI002EQCDANSIE00")
+    static let chartDataURL = URL(string: "https://ae1cdb19-2532-46fa-9b8f-cce01702bb1e.mock.pstmn.io/takehometest/apps/compare/chart?productCodes=KI002MMCDANCAS00&productCodes=TP002EQCEQTCRS00&productCodes=NI002EQCDANSIE00")
+}
